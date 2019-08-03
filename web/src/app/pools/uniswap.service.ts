49eed1f3a77ec29cf4d799d601912e0dd249b152
Anton Bukov <k06aaa@gmail.com>
2019-08-04 03:07:31 +0530

    Add uniswap interest and sllippage

--- web/src/app/pools/uniswap.service.ts
+++ web/src/app/pools/uniswap.service.ts
@@ -51,12 +51,77 @@ export class UniswapService implements PoolInterface {
 
     async interest(tokenAddress: string): Promise<number> {
 
-        return 0;
+        const contract = new ethers.Contract(
+            await this.getExchangeAddress(tokenAddress),
+            UNISWAP_ABI,
+            this.web3Service.provider
+        );
+
+        const currentBlock = await this.web3Service.provider.getBlockNumber();
+        const rawResult = await this.web3Service.provider.getLogs({
+            address: contract.address,
+            fromBlock: currentBlock - 10000,
+            toBlock: currentBlock,
+            topics: [
+                [
+                    contract.filters.AddLiquidity().topics[0],
+                    contract.filters.RemoveLiquidity().topics[0],
+                    contract.filters.EthPurchase().topics[0],
+                    contract.filters.TokenPurchase().topics[0],
+                ]
+            ]
+        });
+
+        const oldBlock = await this.web3Service.provider.getBlock(currentBlock - 10000);
+        const newBlock = await this.web3Service.provider.getBlock(currentBlock);
+        const duration = newBlock.timestamp - oldBlock.timestamp;
+
+        const results = rawResult.map(res => contract.interface.parseLog(res));
+
+        let currentEthBalance = await this.web3Service.provider.getBalance(contract.address);
+        let currentTknBalance = await this.tokenService.getTokenBalanceByAddress(tokenAddress, contract.address);
+        let feePercent = ethers.utils.bigNumberify(0);
+
+        for (let i = results.length - 1; i > 0; i--) {
+
+            if (results[i].topic == contract.filters.AddLiquidity().topics[0]) {
+                currentEthBalance = currentEthBalance.sub(results[i].values.eth_amount);
+                currentTknBalance = currentTknBalance.sub(results[i].values.token_amount);
+            }
+
+            if (results[i].topic == contract.filters.RemoveLiquidity().topics[0]) {
+                currentEthBalance = currentEthBalance.add(results[i].values.eth_amount);
+                currentTknBalance = currentTknBalance.add(results[i].values.token_amount);
+            }
+
+            if (results[i].topic == contract.filters.EthPurchase().topics[0]) {
+                const invariant = currentEthBalance.mul(currentTknBalance);
+                currentEthBalance = currentEthBalance.add(results[i].values.eth_bought);
+                currentTknBalance = currentTknBalance.sub(results[i].values.tokens_sold);
+                const fee = invariant.sub(currentEthBalance.mul(currentTknBalance));
+                feePercent = feePercent.add(fee.mul(1e9).mul(1e9).div(invariant));
+            }
+
+            if (results[i].topic == contract.filters.TokenPurchase().topics[0]) {
+                const invariant = currentEthBalance.mul(currentTknBalance);
+                currentEthBalance = currentEthBalance.sub(results[i].values.eth_sold);
+                currentTknBalance = currentTknBalance.add(results[i].values.tokens_bought);
+                const fee = invariant.sub(currentEthBalance.mul(currentTknBalance));
+                feePercent = feePercent.add(fee.mul(1e9).mul(1e9).div(invariant));
+            }
+        }
+
+        return feePercent.mul(365*24*60*60).div(duration).mul(10000).div(1e9).div(1e9).toNumber() / 100;
     }
 
     async slippage(tokenAddress: string, amount: BigNumber): Promise<number> {
 
-        return 0;
+        const exchangeAddress = await this.getExchangeAddress(tokenAddress);
+        const interest = await this.interest(tokenAddress);
+
+        const balance = await this.tokenService.getTokenBalanceByAddress(tokenAddress, exchangeAddress);
+
+        return balance.mul(Math.floor(interest * 100)).div(balance.add(amount)).toNumber() / 100 - interest;
     }
 
     async deposit(tokenAddress: string, amount: BigNumber) {
