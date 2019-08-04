84ac32ba0856cc64250bb591f0c575548d25d307
deacix <info@deacix.de>
2019-08-04 06:41:00 +0530

    sf

--- web/src/app/pools/lendroid.service.ts
+++ web/src/app/pools/lendroid.service.ts
@@ -1,10 +1,34 @@
 import {Injectable} from '@angular/core';
+import {PoolInterface} from './pool.interface';
+import {BigNumber} from 'ethers/utils';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
-export class LendroidService {
+export class LendroidService implements PoolInterface {
 
-    constructor() {
+    constructor(
+        protected httpClient: HttpClient
+    ) {
+        // https://winged-yeti-201009.appspot.com/offers
+    }
+
+    deposit(tokenSymbol: string, amount: BigNumber) {
+    }
+
+    getBalance(tokenSymbol: string, wallet: string): Promise<BigNumber> {
+        return undefined;
+    }
+
+    getFormatedBalance(tokenSymbol: string, wallet: string): Promise<string> {
+        return undefined;
+    }
+
+    interest(tokenSymbol: string): Promise<number> {
+        return undefined;
+    }
+
+    withdraw(tokenAddress: string, walletAddress: string) {
     }
 }
